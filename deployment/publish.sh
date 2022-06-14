16b518d39cd1681268b08b6dcfdc740db3af4e2c
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-06-14 21:24:14 +0300

    fix(build): Fix variables in publish.sh, exit on error

--- deployment/publish.sh
+++ deployment/publish.sh
@@ -7,12 +7,13 @@ package_name="$1.$full_version.nupkg"
 dotnet pack \
 	./source/"$1"/"$1".csproj \
 	--configuration Release \
-	-p:AssemblyVersion="full_version" \
-	-p:AssemblyFileVersion="full_version" \
-	-p:PackageVersion="full_version" \
+	-p:AssemblyVersion="$full_version" \
+	-p:AssemblyFileVersion="$full_version" \
+	-p:PackageVersion="$full_version" \
 	-p:InformationalVersion="$version""$3" \
 	/warnAsError \
-	/nologo
+	/nologo ||
+	exit
 
 echo "::set-output name=artifact-name::$package_name"
 echo "::set-output name=artifact::$publish_dir/$package_name"
