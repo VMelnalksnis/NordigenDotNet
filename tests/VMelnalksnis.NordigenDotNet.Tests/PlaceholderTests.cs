17df6d32f368a8058e2562ed86bc89a0e20c6536
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-06-14 08:25:51 +0300

    feat: Add project configurations

--- tests/VMelnalksnis.NordigenDotNet.Tests/PlaceholderTests.cs
+++ tests/VMelnalksnis.NordigenDotNet.Tests/PlaceholderTests.cs
@@ -1,4 +1,6 @@
-using Xunit;
+// Copyright 2022 Valters Melnalksnis
+// Licensed under the Apache License 2.0.
+// See LICENSE file in the project root for full license information.
 
 namespace VMelnalksnis.NordigenDotNet.Tests;
 
@@ -7,5 +9,6 @@ public sealed class PlaceholderTests
 	[Fact]
 	public void Pass()
 	{
+		Assert.True(true);
 	}
 }
