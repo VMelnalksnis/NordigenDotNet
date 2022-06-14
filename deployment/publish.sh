e91f1670b52970adb064cf7f339ec4c261981b68
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-06-14 23:44:04 +0300

    fix(build): Fix NuGet package version

--- deployment/publish.sh
+++ deployment/publish.sh
@@ -2,14 +2,14 @@
 version=$(cat version)
 publish_dir="./source/$1/bin/Release"
 full_version="$version.$2"
-package_name="$1.$full_version.nupkg"
+package_name="$1.$version.nupkg"
 
 dotnet pack \
 	./source/"$1"/"$1".csproj \
 	--configuration Release \
 	-p:AssemblyVersion="$full_version" \
 	-p:AssemblyFileVersion="$full_version" \
-	-p:PackageVersion="$full_version" \
+	-p:PackageVersion="$version" \
 	-p:InformationalVersion="$version""$3" \
 	/nologo ||
 	exit
