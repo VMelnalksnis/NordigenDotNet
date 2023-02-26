b75373e697191218835ee13c5b22305c5be0c2a2
Nathaniel Read <nathaniel@northfolk.co.uk>
2023-02-26 13:21:00 +0000

    fix: update TransactionId to be an optional property on pending transactions

--- source/VMelnalksnis.NordigenDotNet/Accounts/PendingTransaction.cs
+++ source/VMelnalksnis.NordigenDotNet/Accounts/PendingTransaction.cs
@@ -2,7 +2,13 @@
 // Licensed under the Apache License 2.0.
 // See LICENSE file in the project root for full license information.
 
+using VMelnalksnis.NordigenDotNet.Institutions;
+
 namespace VMelnalksnis.NordigenDotNet.Accounts;
 
 /// <summary>A transaction that is still pending.</summary>
-public record PendingTransaction : Transaction;
+public record PendingTransaction : Transaction
+{
+	/// <summary>Gets or sets a unique transaction id created by the <see cref="Institution"/>.</summary>
+	public string? TransactionId { get; set; } = null!;
+}
